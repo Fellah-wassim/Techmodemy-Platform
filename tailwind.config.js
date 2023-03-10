8c26287770de3e17ab89deb507913089d7024b41
wassim <wbvb41@gmail.com>
2023-03-10 18:01:07 +0100

    added the assets and changed the favicon, also added the tailwaind to the react app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      green: "#3eb073",
+      darkerGreen: "#29975C",
+      red: "#e83c35",
+      blue: "#3b69b1",
+      yellow: "#fdcf2a",
+      purple: "#76388D",
+    },
   },
   plugins: [],
 };
