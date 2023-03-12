e82310155af6ddb3121402d84e6ed143b034667c
wassim <wbvb41@gmail.com>
2023-03-12 04:13:27 +0100

    fixed a problem when responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,17 +18,20 @@ module.exports = {
       purple: "#76388D",
     },
     screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
       md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
