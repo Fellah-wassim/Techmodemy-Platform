4f5487bf6cc478d76062bdf1fffebd05ec560440
wassim <wbvb41@gmail.com>
2023-03-12 04:43:59 +0100

    changed the font and initialized the training component

---
+++ src/components/Training.js
@@ -0,0 +1,9 @@
+export const Training = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-purple">
+        Discover Our Robotics Training Courses
+      </h1>
+    </div>
+  );
+};
